45fcfa2a24d9a8f91c04a8c206f0e1ec07e13317
megoRU <savin.yura2014@gmail.com>
2020-05-12 17:50:38 +0300

    add parsing

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import java.io.IOException;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    try {
+      Document doc = Jsoup.connect("https://en.wikipedia.org/wiki/Information_asymmetry").get();
 
+      //get page title
+//      String title = doc.title();
+//      System.out.println("title : " + title);
+      Elements mainHeaderElements = doc.select("div#mw-content-text.mw-content-ltr");
+      System.out.println(mainHeaderElements.text());
 
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }

98c4c9d6cb71576f9f8be5d4d09b8794fc1a4d91
megoRU <savin.yura2014@gmail.com>
2020-07-26 17:03:10 +0300

    1. add image app 2. fix bugs 3. optimisation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
@@ -11,6 +12,7 @@ import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.UnsupportedLookAndFeelException;
 import org.jsoup.Jsoup;
@@ -38,7 +40,13 @@ public class Main extends javax.swing.JFrame {
     frame.setLocation(x, y);
     setLocation(x, y);
     super.setTitle("Java HTML parser");
-//    setIconImage(getImage());
+    setIconImage(getImage());
+  }
+
+  private Image getImage() {
+    String fileName = "icon" + ".png";
+    ImageIcon icon = new ImageIcon(getClass().getResource(fileName));
+    return icon.getImage();
   }
 
   private void initComponents() {
